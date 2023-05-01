dcb7156b0efa8807b6743d5903b266aec1903028
douglas <douglasmaxton@gmail.com>
2023-05-01 19:17:02 +0100

    Setup the package.json file to run a terminal command if using neovim

--- server/server.mjs
+++ server/server.mjs
@@ -1,7 +1,6 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-
-import { keys } from "./src/lib/keys.mjs";
+import { keys } from "./src/lib/index.mjs";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -9,43 +8,43 @@ const app = express();
 const { PORT } = keys;
 
 app.get("/feed", async (req, res) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: { author: true },
-  });
-  res.json(posts);
+	const posts = await prisma.post.findMany({
+		where: { published: true },
+		include: { author: true },
+	});
+	res.json(posts);
 });
 
 app.post("/post", async (req, res) => {
-  const { title, content, authorEmail } = req.body;
-  const post = await prisma.post.create({
-    data: {
-      title,
-      content,
-      published: false,
-      author: { connect: { email: authorEmail } },
-    },
-  });
-  res.json(post);
+	const { title, content, authorEmail } = req.body;
+	const post = await prisma.post.create({
+		data: {
+			title,
+			content,
+			published: false,
+			author: { connect: { email: authorEmail } },
+		},
+	});
+	res.json(post);
 });
 
 app.put("/publish/:id", async (req, res) => {
-  const { id } = req.params;
-  const post = await prisma.post.update({
-    where: { id },
-    data: { published: true },
-  });
-  res.json(post);
+	const { id } = req.params;
+	const post = await prisma.post.update({
+		where: { id },
+		data: { published: true },
+	});
+	res.json(post);
 });
 
 app.delete("/user/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await prisma.user.delete({
-    where: {
-      id,
-    },
-  });
-  res.json(user);
+	const { id } = req.params;
+	const user = await prisma.user.delete({
+		where: {
+			id,
+		},
+	});
+	res.json(user);
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
