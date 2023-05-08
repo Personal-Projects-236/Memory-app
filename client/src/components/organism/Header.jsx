267c5fd9eccd166217b05991e7fd210377ff7eed
douglas <douglasmaxton@gmail.com>
2023-05-08 16:25:27 +0100

    Created a header component with some basic styling

---
+++ client/src/components/organism/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/components/organism/Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.div}>
+      <h1 className={styles.h1}>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
