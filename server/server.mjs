7ddce93e04160297181d521c85c0beb261110362
douglas <douglasmaxton@gmail.com>
2023-05-17 15:19:05 +0100

    Changed the Schema file for storing data for form and del all existing endpoints

--- server/server.mjs
+++ server/server.mjs
@@ -7,44 +7,8 @@ const app = express();
 
 const { PORT } = keys;
 
-app.get("/feed", async (req, res) => {
-	const posts = await prisma.post.findMany({
-		where: { published: true },
-		include: { author: true },
-	});
-	res.json(posts);
-});
-
-app.post("/post", async (req, res) => {
-	const { title, content, authorEmail } = req.body;
-	const post = await prisma.post.create({
-		data: {
-			title,
-			content,
-			published: false,
-			author: { connect: { email: authorEmail } },
-		},
-	});
-	res.json(post);
-});
-
-app.put("/publish/:id", async (req, res) => {
-	const { id } = req.params;
-	const post = await prisma.post.update({
-		where: { id },
-		data: { published: true },
-	});
-	res.json(post);
-});
-
-app.delete("/user/:id", async (req, res) => {
-	const { id } = req.params;
-	const user = await prisma.user.delete({
-		where: {
-			id,
-		},
-	});
-	res.json(user);
+app.get("/", (req, res) => {
+	res.send("Hello");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
