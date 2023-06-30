dad279d7e9a4fccf37bfa3ac41f4dba367d616b7
douglas <douglasmaxton@gmail.com>
2023-06-30 12:58:54 +0100

    Created the delete endpoint for cards based on query of the id number

--- server/src/router/cardRoutes.mjs
+++ server/src/router/cardRoutes.mjs
@@ -1,10 +1,31 @@
 import express from "express";
 import cardControllers from "../controllers/cardControllers.mjs";
 
-const router = express.Router();
+import { PrismaClient } from "@prisma/client";
 
-router.get("/", async (req, res) => {
-	await cardControllers(req, res);
-});
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router
+	.get("/", async (req, res) => {
+		await cardControllers(req, res);
+	})
+	.delete("/", async (req, res) => {
+		const { query } = req;
+		const { id } = query;
+
+		await prisma.user
+			.delete({
+				where: {
+					id: parseInt(id),
+				},
+			})
+			.then(() => {
+				res
+					.status(200)
+					.json({ msg: "Your Card has been successfully deleted" });
+			})
+			.catch((err) => console.log("err", err));
+	});
 
 export default router;
