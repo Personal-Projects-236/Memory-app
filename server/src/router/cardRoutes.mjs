67158a7f79c7c53b734a6f0715e9fc056dd494a7
douglas <douglasmaxton@gmail.com>
2023-06-30 18:07:41 +0100

    created the update endpoint

--- server/src/router/cardRoutes.mjs
+++ server/src/router/cardRoutes.mjs
@@ -1,13 +1,27 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 import getCard from "../controllers/cardControllers/getCard.mjs";
 import deleteCard from "../controllers/cardControllers/deleteCard.mjs";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 router
 	.get("/", async (req, res) => {
 		await getCard(req, res);
+	})
+	.put("/", async (req, res) => {
+		const { body, query } = req;
+		const { id } = query;
+
+		await prisma.user
+			.update({
+				where: { id: parseInt(id) },
+				data: body,
+			})
+			.then(() => console.log("Data successfully updated"))
+			.catch((err) => console.log(err));
 	})
 	.delete("/", async (req, res) => {
 		await deleteCard(req, res);
